2f81a37f27eba900ae3c7ab66d2b332da3f2fa05
jabber <2898117012@qq.com>
2022-01-20 18:27:15 +0800

    factory

--- database/factories/CountryFactory.php
+++ database/factories/CountryFactory.php
@@ -12,7 +12,22 @@ class CountryFactory extends Factory
     public function definition()
     {
         return [
-
+            'code' => $this->faker->countryCode(),
+            'name' => $this->faker->country(),
+            'alpha2' => $this->faker->countryCode(),
+            'alpha3' => $this->faker->countryISOAlpha3(),
+            'currency' => $this->faker->currencyCode(),
+            'currency_code' => $this->faker->currencyCode(),
+            'currency_sub_unit' => $this->faker->currencyCode(),
+            'currency_symbol' => $this->faker->currencyCode(),
+            'currency_decimals' => $this->faker->biasedNumberBetween(0, 2),
+            'currency_rate' => $this->faker->randomFloat(2, 0, 100),
+            'flag' => $this->faker->countryCode(),
+            'calling_code' => $this->faker->biasedNumberBetween(0, 10000) . '',
+            'region_code' => $this->faker->biasedNumberBetween(0, 10000) . '',
+            'sub_region_code' => $this->faker->biasedNumberBetween(0, 10000) . '',
+            'locale' => $this->faker->locale(),
+            'eea' => true
         ];
     }
 }
