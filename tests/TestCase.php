a72179a2f2b364a4afc259247c6b7517d2533011
jabber <2898117012@qq.com>
2022-02-08 11:46:23 +0800

    fix: test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,9 @@
 
 namespace Dealskoo\Country\Tests;
 
-use Dealskoo\Admin\Providers\AdminServiceProvider;
 use Dealskoo\Country\Providers\CountryServiceProvider;
-use Dealskoo\Country\Tests\Http\Kernel;
-use Illuminate\Encryption\Encrypter;
 
-abstract class TestCase extends \Orchestra\Testbench\TestCase
+abstract class TestCase extends \Dealskoo\Admin\Tests\TestCase
 {
     protected function getPackageProviders($app)
     {
@@ -16,46 +13,9 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         ];
     }
 
-    public function ignorePackageDiscoveriesFrom()
-    {
-        return [];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('app.key', 'base64:' . base64_encode(Encrypter::generateKey('AES-256-CBC')));
-        $app['config']->set('database.default', 'testing');
-        $app['config']->set('database.connections.testing', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => ''
-        ]);
-        $app['config']->set('auth.guards.admin', [
-            'driver' => 'session',
-            'provider' => 'admins',
-        ]);
-        $app['config']->set('auth.providers.admins', [
-            'driver' => 'eloquent',
-            'model' => \Dealskoo\Admin\Models\Admin::class,
-        ]);
-        $app['config']->set('auth.passwords.admins', [
-            'provider' => 'admins',
-            'table' => 'admin_password_resets',
-            'expire' => 60,
-            'throttle' => 60,
-        ]);
-        $app['config']->set('auth.password_length', 8);
-    }
-
-    protected function resolveApplicationHttpKernel($app)
-    {
-        $app->singleton(\Illuminate\Contracts\Http\Kernel::class, Kernel::class);
-    }
-
     protected function setUp(): void
     {
         parent::setUp();
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadMigrationsFrom(__DIR__ . '/migrations');
     }
 }
