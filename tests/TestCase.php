86b9e9d8e7babd193e62dce275be6350f3e4d9bf
jabber <2898117012@qq.com>
2022-02-14 15:14:26 +0800

    fix: test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,6 @@
 
 namespace Dealskoo\Country\Tests;
 
-use Dealskoo\Country\Providers\CountryServiceProvider;
-
 abstract class TestCase extends \Dealskoo\Admin\Tests\TestCase
 {
-    protected function getPackageProviders($app)
-    {
-        return [
-            CountryServiceProvider::class
-        ];
-    }
 }
